07f2b1ce065d13f1945053b93df702a7484dba5d
bumbeishvili <davit.bumbeishvili@gmail.com>
2020-05-05 01:45:54 +0400

    Updates

---
+++ precache-manifest.4516ce8c975e12029dc01e55f5a65225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ade28b6bf4e727a644b4184f87ddcf0",
+    "url": "/react-github-deploy-boilerplate-gh-pages/index.html"
+  },
+  {
+    "revision": "4b165fd999413c21e747",
+    "url": "/react-github-deploy-boilerplate-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4052c61b915bf67e3f8e",
+    "url": "/react-github-deploy-boilerplate-gh-pages/static/js/2.feda3f98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-deploy-boilerplate-gh-pages/static/js/2.feda3f98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b165fd999413c21e747",
+    "url": "/react-github-deploy-boilerplate-gh-pages/static/js/main.61084923.chunk.js"
+  },
+  {
+    "revision": "415955b279becf15d51c",
+    "url": "/react-github-deploy-boilerplate-gh-pages/static/js/runtime-main.fca7e08e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-deploy-boilerplate-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
